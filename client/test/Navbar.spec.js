80ee2bf7c4c8f1e37ea827119e58aee8528ca14f
Ryan <ryan.holton@stsonline.uk.com>
2020-02-16 17:26:42 +0000

    :rotating_light: remove tests as not really needed for this project

--- client/test/Navbar.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Navbar from '@/components/Navbar.vue'
-
-describe('Navbar', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Navbar, { store })
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
